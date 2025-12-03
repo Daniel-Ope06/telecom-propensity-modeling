48760f406695d7fc8eb4297ff2e337b7c414e8b0
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2025-12-03 20:36:11 +0000

    refactor: Update `load_wallace_data()` to accept `Path` instead of `str`

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from pandas import Series
+from pathlib import Path
 
 
-def load_wallace_data(filepath: str) -> pd.DataFrame:
+def load_wallace_data(filepath: Path) -> pd.DataFrame:
     """Loads raw data from a CSV file."""
     return pd.read_csv(filepath)
 
